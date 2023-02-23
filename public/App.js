b0bf60daa322a243d3b5e6150869cb80624cb095
XIII <minibrusp@gmail.com>
2023-02-24 01:59:29 +0800

    Added tabbing feature

--- public/App.js
+++ public/App.js
@@ -6,6 +6,7 @@ let totalFollowers = document.querySelector('.total-followers')
 let valuesToday = document.querySelectorAll('.value-today')
 let overviewMainValues = document.querySelectorAll('#overview strong')
 let overviewValuesToday = document.querySelectorAll('#overview .overview-today')
+let themeLabel = document.querySelector('#theme-label')
 
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -18,14 +19,20 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 checkbox.addEventListener('click', () => {
-    
-    if(checkbox.checked === true) {
-        document.documentElement.classList.add('dark')
-        localStorage.theme = 'dark'
-        return
-    } 
-    document.documentElement.classList.remove('dark')
-    localStorage.theme = 'light'
+    checkboxValue()
+})
+
+themeLabel.addEventListener('keyup', event => {
+    if(event.key === ' ' || event.key === 'Enter') {
+        event.preventDefault()
+        checkbox.checked ? checkbox.checked = false : checkbox.checked = true
+        checkboxValue()
+    }
+})
+themeLabel.addEventListener('keydown', event => {
+    if(event.key === ' ') {
+        event.preventDefault()
+    }
 })
 
 
@@ -48,3 +55,14 @@ overviewMainValues.forEach(value => {
 overviewValuesToday.forEach(value => {
     numberAnimationZeroUp(value)
 })
+
+
+function checkboxValue() {
+    if(checkbox.checked === true) {
+        document.documentElement.classList.add('dark')
+        localStorage.theme = 'dark'
+        return
+    } 
+    document.documentElement.classList.remove('dark')
+    localStorage.theme = 'light'
+}
